fd2da59dfd8849c87970a1b5e1372e68c40d441c
aliona <petrova.alja@yandex.ru>
2020-04-23 13:45:13 +0300

    add file name validation

--- gui/recordform.py
+++ gui/recordform.py
@@ -27,8 +27,11 @@ class RecordForm(QtWidgets.QFrame):
         self.dirname = str(pathlib.Path().absolute())
         self.dirname_lbl = QtWidgets.QLabel(self.dirname)
         self.filename_edit = QtWidgets.QLineEdit('')
+        self.file_check_status = QtWidgets.QLabel('')
         self.dir_edit_btt.clicked.connect(self.show_select_dir_dialog)
         self.dir_edit_btt.setProperty('class', 'browse-btt')
+        self.filename_edit.textEdited.connect(self.check_file_status)
+        self.file_check_status.setProperty('class', 'warn-lbl')
 
         outputsettings_layout = QtWidgets.QVBoxLayout()
         filesettings_layout = QtWidgets.QHBoxLayout()
@@ -36,6 +39,10 @@ class RecordForm(QtWidgets.QFrame):
 
         filesettings_layout.addWidget(QtWidgets.QLabel('Filename:'))
         filesettings_layout.addWidget(self.filename_edit)
+        filesettings_layout.addWidget(self.file_check_status)
+        filesettings_layout.setAlignment(self.filename_edit, Qt.AlignLeft)
+        filesettings_layout.setAlignment(self.file_check_status, Qt.AlignLeft)
+        filesettings_layout.addStretch(1)
 
         dirsettings_layout.addWidget(QtWidgets.QLabel('Directory:'))
         dirsettings_layout.addWidget(self.dirname_lbl)
@@ -62,13 +69,14 @@ class RecordForm(QtWidgets.QFrame):
 
     def on_rec_clicked(self):
         if self.record_status == 0:
-            self.record_status = 1
             self.start_record_process()
         else:
-            self.record_status = 0
             self.stop_record_process()
 
     def start_record_process(self):
+        if not self.check_file_status(self.filename_edit.text()):
+            return
+        self.record_status = 1
         self.record_btt.setText('Stop')
         self.filename_edit.setEnabled(False)
         self.record_status_lbl.setText('Recording to file ' + self.filename_edit.text() + ' ...')
@@ -77,6 +85,7 @@ class RecordForm(QtWidgets.QFrame):
         self.seconds_recorded = 0
 
     def stop_record_process(self):
+        self.record_status = 0
         self.record_btt.setText('Start')
         self.filename_edit.setEnabled(True)
         self.record_status_lbl.setText('Record completed: ' + self.filename_edit.text() + ' .')
@@ -84,9 +93,24 @@ class RecordForm(QtWidgets.QFrame):
         self.record_timer.stop()
 
     def show_select_dir_dialog(self):
-        self.dirname = QtWidgets.QFileDialog.getExistingDirectory(self, "Select directory", directory=self.dirname)
+        dirname = QtWidgets.QFileDialog.getExistingDirectory(self, "Select directory", directory=self.dirname)
+        if dirname != '':
+            self.dirname = dirname
         self.dirname_lbl.setText(self.dirname)
 
     def update_time_lbl(self):
         self.seconds_recorded += 1
         self.record_time_lbl.setText(str(datetime.timedelta(seconds=self.seconds_recorded)))
+
+    def check_file_status(self, filename):
+        if filename == '':
+            self.file_check_status.setText('Enter file name')
+            return False
+        if (pathlib.Path(self.dirname) / filename).is_file():
+            self.file_check_status.setText('File already exists. Its contents will be overwritten.')
+            return True
+        if (pathlib.Path(self.dirname) / filename).is_dir():
+            self.file_check_status.setText('Directory names are not allowed.')
+            return False
+        self.file_check_status.setText('')
+        return True
