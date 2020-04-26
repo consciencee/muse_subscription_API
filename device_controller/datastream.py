5461a511226eb123e29a75eed8c2f440f5763528
aliona <petrova.alja@yandex.ru>
2020-04-26 16:30:09 +0300

    add check for running state

--- device_controller/datastream.py
+++ device_controller/datastream.py
@@ -2,20 +2,25 @@ from muselsl import stream
 from multiprocessing import set_start_method, Process
 from collections import namedtuple
 
-RUNNING = 1
-STOPPED = 0
-NON_INIT = -1
+
 
 SensorPreset = namedtuple('SensorPreset', 'gyro_enabled acc_enabled ppg_enabled eeg_disabled')
 
 
 class Datastream:
+    RUNNING = 1
+    STOPPED = 0
+    NON_INIT = -1
+
     def __init__(self, address, sensor_preset=SensorPreset(False, False, False, False)):
         self.process = None
-        self.state = NON_INIT
+        self.state = Datastream.NON_INIT
         self.address = address
         self.sensor_preset = sensor_preset
         print("Created stream for %s" % address)
+
+    def is_running(self):
+        return self.state == Datastream.RUNNING
 
     def set_acc_enabled(self, enabled):
         self.sensor_preset = SensorPreset(
@@ -61,11 +66,12 @@ class Datastream:
                                    self.sensor_preset.gyro_enabled,
                                    self.sensor_preset.eeg_disabled
                                ))
-        self.state = STOPPED
+        self.state = Datastream.STOPPED
         self.process.start()
-        self.state = RUNNING
+        self.state = Datastream.RUNNING
         print("Running stream for %s" % self.address)
 
     def stop(self):
         self.process.terminate()
-        self.state = STOPPED
+        print("Stopped stream for %s" % self.address)
+        self.state = Datastream.STOPPED
