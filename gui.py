50b263c55f3bc8f804a7a663563140b01736d71f
aliona <petrova.alja@yandex.ru>
2020-04-26 15:57:28 +0300

    write headers to data file

--- gui.py
+++ gui.py
@@ -94,7 +94,7 @@ class MuseQtHandler(QObject):
         self.quality_received.emit(qualified_data, channel)
 
         if self.is_recording:
-            self.fileRecorder.append(data, timestamp)
+            self.fileRecorder.append(data, timestamp, headers=channel.ch_names)
 
 class MainWindow(QtWidgets.QMainWindow):
 
--- plugins/filewriter.py
+++ plugins/filewriter.py
@@ -3,11 +3,16 @@ import pathlib
 class FileWriter:
     def __init__(self):
         self.fh = None
+        self.headers_written = False
 
     def open(self, dirname, filename):
         self.fh = open(str(pathlib.Path(dirname) / filename), 'w+')
+        self.headers_written = False
 
-    def append(self, data, timestamp):
+    def append(self, data, timestamp, headers):
+        if not self.headers_written:
+            self.fh.write('Timestamp, ' + ', '.join(map(str, headers)) + '\n')
+            self.headers_written = True
         for i in range(len(data)):
             self.fh.write(str(timestamp[i]) + ', ' + ', '.join(map(str, data[i])) + '\n')
 
